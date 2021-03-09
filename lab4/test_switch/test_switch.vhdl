49d79fd91097ece29e9a8c948a838c5a650d9f81
Luke Taylor <luke@deentaylor.com>
2021-03-09 13:49:50 -0500

    Test push button switches in test_switch

--- lab4/test_switch/test_switch.vhdl
+++ lab4/test_switch/test_switch.vhdl
@@ -3,6 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity test_switch is
   port(
+    -- DP switches 1–8
     gpio_2: in std_logic;
     gpio_46: in std_logic;
     gpio_47: in std_logic;
@@ -11,12 +12,20 @@ entity test_switch is
     gpio_4: in std_logic;
     gpio_44: in std_logic;
     gpio_6: in std_logic;
-    led_r : out std_logic
+    -- Push button switches
+    gpio_28: in std_logic;
+    gpio_38: in std_logic;
+    -- Indicator LEDs
+    led_r : out std_logic;
+    led_g : out std_logic;
+    led_b : out std_logic
   );
 end test_switch;
 
 architecture synth of test_switch is
 begin
-  -- Change this to test a different switch
+  led_g <= gpio_28;
+  led_b <= gpio_38;
+  -- Change this to test a different DP switch
   led_r <= gpio_6;
 end;
