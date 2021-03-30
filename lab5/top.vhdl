e3ad77e2bbc391dffca402009b74622dfb2db6d8
Luke Taylor <luke@deentaylor.com>
2021-03-30 14:59:05 -0400

    Lab 5: Two-digit display (attempt 1; broken)

--- lab5/top.vhdl
+++ lab5/top.vhdl
@@ -27,6 +27,17 @@ end top;
 
 
 architecture synth of top is
+  -- Clock
+  component SB_HFOSC is
+    generic (CLKHF_DIV : String := "0b00");
+    port(
+      CLKHFPU : in std_logic := 'X'; -- Set to 1 to power up
+      CLKHFEN : in std_logic := 'X'; -- Set to 1 to enable output
+      CLKHF : out std_logic := 'X' -- Clock output
+    );
+  end component;
+  signal clk: std_logic;
+
   -- Dual digit decimal driver
   component dddd is
     port(
@@ -35,27 +46,50 @@ architecture synth of top is
       onesdigit : out std_logic_vector(6 downto 0)
     );
   end component;
-
   signal value : unsigned(5 downto 0);
   signal tensdigit_segments : std_logic_vector(6 downto 0);
   signal onesdigit_segments : std_logic_vector(6 downto 0);
+  signal display_tens : std_logic := '0';
 begin
-  -- Note: we invert everything because it’s configured as pull-up
+  -- Initialize clock
+  osc: SB_HFOSC
+    generic map(CLKHF_DIV => "0b00")
+    port map(CLKHFPU => '1', CLKHFEN => '1', CLKHF => clk);
 
   -- Set up 6-bit input number from DP switch inputs
+  -- Note: we invert everything because it’s configured as pull-up
   value <= not inp_5 & not inp_4 & not inp_3 & not inp_2 & not inp_1 & not inp_0;
 
   -- Decode number
   DDDD_1 : dddd port map(value => value, tensdigit => tensdigit_segments, onesdigit => onesdigit_segments);
 
   -- Hook up seven-bit number to seven segments
-  display_1 <= '1';
-  display_2 <= '1';
-  seg_a <= onesdigit_segments(6);
-  seg_b <= onesdigit_segments(5);
-  seg_c <= onesdigit_segments(4);
-  seg_d <= onesdigit_segments(3);
-  seg_e <= onesdigit_segments(2);
-  seg_f <= onesdigit_segments(1);
-  seg_g <= onesdigit_segments(0);
+  display_1 <= display_tens;
+  display_2 <= not display_tens;
+  process(clk) begin
+    if rising_edge(clk) then
+      -- Display either ones digit or tens digit depending on which display is powered
+      -- Note: if we have display_tens = '1', that means it's about to flip to zero, so we should
+      --       display the ones digit, not the tens digit.
+      if display_tens = '1' then
+        seg_a <= onesdigit_segments(6);
+        seg_b <= onesdigit_segments(5);
+        seg_c <= onesdigit_segments(4);
+        seg_d <= onesdigit_segments(3);
+        seg_e <= onesdigit_segments(2);
+        seg_f <= onesdigit_segments(1);
+        seg_g <= onesdigit_segments(0);
+      else
+        seg_a <= tensdigit_segments(6);
+        seg_b <= tensdigit_segments(5);
+        seg_c <= tensdigit_segments(4);
+        seg_d <= tensdigit_segments(3);
+        seg_e <= tensdigit_segments(2);
+        seg_f <= tensdigit_segments(1);
+        seg_g <= tensdigit_segments(0);
+      end if;
+      -- Flip which display is powered
+      display_tens <= not display_tens;
+    end if;
+  end process;
 end;
