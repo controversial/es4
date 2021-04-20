901bddc847d7a3664694712d27d878cd036cc20e
Luke Taylor <luke@deentaylor.com>
2021-04-20 14:29:19 -0400

    Use vhdl 08 for lab 6

--- lab6/build.sh
+++ lab6/build.sh
@@ -4,9 +4,9 @@ echo "Building with ${PROGRAM_NAME}"
 rm *.json *.asc *.bin *.cf
 
 # Simulate
-ghdl -a *.vhdl || exit 1
+ghdl -a --std=08 *.vhdl || exit 1
 # Synthesize (and generate outputs for SVG render)
-yosys -p "ghdl ${PROGRAM_NAME}; synth_ice40 -json ${PROGRAM_NAME}.json;" || exit 1
+yosys -p "ghdl --std=08 ${PROGRAM_NAME}; synth_ice40 -json ${PROGRAM_NAME}.json;" || exit 1
 # Route, generate, and flash
 nextpnr-ice40 --up5k --package sg48 --pcf pins.pcf --json "${PROGRAM_NAME}.json" --asc "${PROGRAM_NAME}.asc" || exit 1
 icepack "${PROGRAM_NAME}.asc" "${PROGRAM_NAME}.bin" || exit 1
