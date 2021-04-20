ba90542370c9cf2b62a27ecc842b82eac257c3fc
Luke Taylor <luke@deentaylor.com>
2021-04-20 14:32:21 -0400

    Turn off RGB pixels during non-visible period

--- lab6/vga.vhdl
+++ lab6/vga.vhdl
@@ -28,6 +28,8 @@ architecture synth of vga is
 
   signal col_counter : unsigned(9 downto 0) := "0000000000";
   signal row_counter : unsigned(9 downto 0) := "0000000000";
+  signal row_visible : std_logic;
+  signal col_visible : std_logic;
 begin
   pll1: pll port map(
     clk_in => clk,
@@ -49,16 +51,17 @@ begin
   vsync <= '1' when row_counter < 2 else '0';
 
   -- Row and col encode pixel positions, only in the visible area
-  row <= (row_counter - (2 + 33)) when (row_counter >= 2 + 33) and (row_counter < (2 + 33 + 480))
-         else "0000000000";
-  col <= (col_counter - (96 + 48)) when (col_counter >= 96 + 48) and (col_counter < (96 + 48 + 640))
-         else "0000000000";
+  row_visible <= '1' when ((row_counter >= 2 + 33) and (row_counter < 2 + 33 + 480)) else '0';
+  col_visible <= '1' when ((col_counter >= 96 + 48) and (col_counter < 96 + 48 + 640)) else '0';
+
+  row <= row_counter - (2 + 33) when row_visible else "0000000000";
+  col <= col_counter - (96 + 48) when col_visible else "0000000000";
 
   -- Unpack individual pixel values from 6-bit vector
-  red1 <= rgb(5);
-  red0 <= rgb(4);
-  grn1 <= rgb(3);
-  grn0 <= rgb(2);
-  blu1 <= rgb(1);
-  blu0 <= rgb(0);
+  red1 <= rgb(5) when row_visible and col_visible else '0';
+  red0 <= rgb(4) when row_visible and col_visible else '0';
+  grn1 <= rgb(3) when row_visible and col_visible else '0';
+  grn0 <= rgb(2) when row_visible and col_visible else '0';
+  blu1 <= rgb(1) when row_visible and col_visible else '0';
+  blu0 <= rgb(0) when row_visible and col_visible else '0';
 end;
