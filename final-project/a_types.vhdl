42c882a6d0f79bd76408f5a7e147af1990167163
Luke Taylor <luke@deentaylor.com>
2021-05-10 23:03:47 -0400

    Add comment to types

--- final-project/a_types.vhdl
+++ final-project/a_types.vhdl
@@ -1,3 +1,5 @@
+-- needs a in filename to go first alphabetically so that this definition is available for
+-- subsequent files
 package snake_types is
   type direction is (NORTH, EAST, SOUTH, WEST, NONE);
 end package;
